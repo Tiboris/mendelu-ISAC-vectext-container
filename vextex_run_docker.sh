283e60642ff511a79d6b97abb8f692a9aad36ec1
Tibor Dudlák <tdudlak@redhat.com>
2020-03-27 20:36:13 +0100

    fix script
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- vextex_run_docker.sh
+++ vextex_run_docker.sh
@@ -1,1 +1,1 @@
-docker run -ti --rm -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix  --name my-vectext vectex
+docker run -ti --rm -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix  --name my-vectext vectext
